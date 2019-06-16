ddf776721a8beae11624ce6a8f0ba7eb765f34f0
typewriter1 <40742741+typewriter1@users.noreply.github.com>
2019-06-16 14:49:07 +0100

    Change sample to use systems as classes

--- examples/panda3d_sample.py
+++ examples/panda3d_sample.py
@@ -7,15 +7,22 @@ class ModelComponent:
     def __init__(self, path, parent):
         self.model = loader.load_model(path)
         self.model.reparent_to(parent)
-        
-    def cleanup(self):
-        self.model.remove_node()
           
 class PosComponent:
     def __init__(self, x=0, y=0, z=0):
         self.x = x
         self.y = y
         self.z = z
+        
+class PosSystem:
+    def __init__(self, ecs):
+        self.ecs = ecs
+        
+    def update(self):
+        for entity in self.ecs.get_entities_with_component(ModelComponent, PosComponent):
+            model = self.ecs.get_component(entity, ModelComponent)
+            pos = self.ecs.get_component(entity, PosComponent)
+            model.model.set_pos(pos.x, pos.y, pos.z)
 
 class Demo(ShowBase):
     def __init__(self):
@@ -24,17 +31,11 @@ class Demo(ShowBase):
         self.ecs = ecs = compynent.EntityManager()
         self.player = self.ecs.create_entity()
         self.ecs.add_component(self.player, ModelComponent("models/box.egg", self.render))
-        self.ecs.add_component(self.player, PosComponent(x = 0, y = 20, z = 0))
+        self.ecs.add_component(self.player, PosComponent(x = 0, y = 0, z = 5))
 
-        ecs.add_system(self.pos_system)
+        ecs.add_system(PosSystem(self.ecs))
         self.task_mgr.add(self.frame_update_task, "Do ECS Frame")
-        
-    def pos_system(self):
-        for entity in self.ecs.get_entities_with_component(ModelComponent, PosComponent):
-            model = self.ecs.get_component(entity, ModelComponent)
-            pos = self.ecs.get_component(entity, PosComponent)
-            model.model.set_pos(pos.x, pos.y, pos.z)
-            
+                   
     def frame_update_task(self, task):
         self.ecs.do_frame()
         return task.cont
